beba634fbaf601e4c71b0fe4e868861fcfa803ed
jdugue6 <59034165+jdugue6@users.noreply.github.com>
2020-04-19 19:12:12 -0400

    Added new files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from './Components/Header';
+import Home from './Components/Home';
+const mapStateToProps = state => ({
+
+     appName: state.appName
+     });
+    
+
+class App extends React.Component{
+    
+    render() {
+        return (
+            <div>
+            <Header appName={this.props.appName} />
+            <Home></Home>
+            </div>
+        );
+      }
+}
+
+export default connect(mapStateToProps, () => ({}) )(App);
